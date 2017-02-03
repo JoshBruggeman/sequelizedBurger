32d43a1951ddc0fc812c393eddb054f8460b8afd
Josh Bruggeman <joshrbruggeman@gmail.com>
2017-02-03 11:19:01 -0600

    Updating Sequelize

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,18 @@
-var orm = require("../config/orm");
-
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(name, cb) {
-    orm.create("burgers", ["burger_name", "devoured"], [name, false], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
-  }
+module.exports = function(sequelize, DataTypes) {
+  
+  var Burger = sequelize.define("Burger", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    burger_name: {
+      type: DataTypes.STRING
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN = false
+    }
+    
+  });
+  return Burger;
 };
-
-module.exports = burger;
