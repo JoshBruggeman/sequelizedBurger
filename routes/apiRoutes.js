8d279660be9b0c3b22f543e5d35f43456d7d396a
Josh Bruggeman <joshrbruggeman@gmail.com>
2017-02-03 12:39:57 -0600

    Routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,36 @@
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the posts
+    app.get("/api", function(req, res) {
+      var query = {};
+      db.Burger.findAll({}).then(function(dbBurg) {
+        console.log("db Burg data: " + dbBurg);
+        res.json(dbBurg);
+      });
+    });
+
+  // POST route for saving a new post
+  app.post("/api/burgers", function(req, res) {
+    db.Burger.create(req.body).then(function(dbBurg) {
+      res.json(dbBurg);
+    });
+  });
+
+  // PUT route for updating burgers
+  app.put("/api/posts", function(req, res) {
+    db.Post.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+
+};
